6dfa3c47521f6807fd07d226c489edbed86168ad
Caleb Howard <choward@covermymeds.com>
2019-02-16 15:51:47 -0500

    Clean up location imports

--- tracker/views/location_views.py
+++ tracker/views/location_views.py
@@ -1,17 +1,10 @@
-from ..forms import DateRangeForm, SignUpForm, AddLocationForm, CreateJournalForm, DateRecordForm, UpdateDateRecordForm, DateRecordNotesForm, UpdatePrecipRecordForm , UpdateShareForm, HomeLocationForm
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from ..models import User, Location, Journal, Date_record, Precip_record, Date_record_note, Share, Current_location
-from calendar import HTMLCalendar, monthrange
-from django.db.models.functions import Lower
-from datetime import datetime, date, timedelta
+from ..forms import AddLocationForm, HomeLocationForm
+from ..models import User, Location, Current_location
+from datetime import datetime
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Sum
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render, render_to_response
-from django.utils.html import conditional_escape as esc
+from django.shortcuts import redirect, render, render_to_response
 from django.utils.safestring import mark_safe
 from django.views import generic, View
 from itertools import groupby
